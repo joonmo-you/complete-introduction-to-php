7e782c641b297cc1ac01c70278e1574cd0a372c0
joonmo-you <joonmo.you@gmail.com>
2023-05-21 17:20:33 +0900

    PHP data type assignments

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php 
 $title = 'My Blog';
+$numberOfPosts = 10;
 ?>
 
 <h1><?= $title ?></h1>
+<h2><?= $numberOfPosts ?></h2>
+
+<p><?= gettype($title) ?></p>
+<p><?= gettype($numberOfPosts) ?></p>
