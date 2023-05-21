4f6843feb25d46555427820e0bfd107f8b0f0699
joonmo-you <joonmo.you@gmail.com>
2023-05-21 17:56:25 +0900

    Create a Boolean variable in PHP

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $title = 'My Blog';
 $numberOfPosts = 10;
 $numberOfPostsText = "\"$numberOfPosts\" posts";
-
+$hasPosts = $numberOfPosts > 0;
 define('MIN_NUM_POSTS', 0);
 const MAX_NUM_POSTS = 100;
 ?>
@@ -11,3 +11,4 @@ const MAX_NUM_POSTS = 100;
 <h2><?= $numberOfPostsText ?></h2>
 <h3>Minimum: <?= MIN_NUM_POSTS ?></h3>
 <h3>Maximum: <?= MAX_NUM_POSTS ?></h3>
+<?php var_dump($hasPosts) ?>
