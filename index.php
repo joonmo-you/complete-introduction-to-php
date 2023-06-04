c89d756d977c54359355e5eec63f8ec2985259d7
joonmo-you <joonmo.you@gmail.com>
2023-06-04 17:29:34 +0900

    Write multiple conditions to execute alternate PHP code

--- index.php
+++ index.php
@@ -10,7 +10,11 @@ const MAX_NUM_POSTS = 100;
 <h1><?= $title ?></h1>
 <h2><?= $numberOfPostsText ?></h2>
 <?php
-if ($hasPosts) {
+if ($numberOfPosts === 3) {
+  echo "There are exactly 3 posts.";
+} elseif ($hasPosts) {
   echo "Post exists.";
+} else {
+  echo "There are no posts.";
 }
 ?>
