770de1a1d4075813791aad11ce3ba2ae21b4a755
joonmo-you <joonmo.you@gmail.com>
2023-06-04 17:43:20 +0900

    Refactor a PHP if else statement into a ternary

--- index.php
+++ index.php
@@ -9,12 +9,4 @@ const MAX_NUM_POSTS = 100;
 
 <h1><?= $title ?></h1>
 <h2><?= $numberOfPostsText ?></h2>
-<?php if ($hasPosts): ?>
-  <?php if ($numberOfPosts === 3): ?>
-    There are exactly 3 posts.
-  <?php else: ?>
-    Posts exists.
-  <?php endif ?>
-<?php else: ?>
-      There are no posts.
-<?php endif ?>
+<?= $hasPosts ? 'Posts exists.' : 'There are no exists.' ?>
