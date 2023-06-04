f75d250f2501aaf29d14c815e4863b64cb5d7f9d
joonmo-you <joonmo.you@gmail.com>
2023-06-04 17:49:43 +0900

    Use a PHP switch statement for advanced conditional logic

--- index.php
+++ index.php
@@ -5,8 +5,20 @@ $numberOfPostsText = "\"$numberOfPosts\" posts";
 $hasPosts = $numberOfPosts > 0;
 define('MIN_NUM_POSTS', 0);
 const MAX_NUM_POSTS = 100;
+switch ($numberOfPosts) {
+  case 0: 
+    $message = 'There are no posts.';
+    break;
+  case 1:
+  case 2:
+  case 3:
+    $message = 'There are a few posts.';
+    break;
+  default: 
+    $message = 'There are many posts.';
+}
 ?>
 
 <h1><?= $title ?></h1>
 <h2><?= $numberOfPostsText ?></h2>
-<?= $hasPosts ? 'Posts exists.' : 'There are no exists.' ?>
+<p><?= $message ?></p>
