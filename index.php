4c0c4941733fa6b56022a67415833a2ea1d9b390
joonmo-you <joonmo.you@gmail.com>
2023-06-04 17:57:41 +0900

    Use a match statement for advanced PHP data checks

--- index.php
+++ index.php
@@ -5,20 +5,12 @@ $numberOfPostsText = "\"$numberOfPosts\" posts";
 $hasPosts = $numberOfPosts > 0;
 define('MIN_NUM_POSTS', 0);
 const MAX_NUM_POSTS = 100;
-switch ($numberOfPosts) {
-  case 0: 
-    $message = 'There are no posts.';
-    break;
-  case 1:
-  case 2:
-  case 3:
-    $message = 'There are a few posts.';
-    break;
-  default: 
-    $message = 'There are many posts.';
+$message = match ($numberOfPosts) {
+  0 => 'There are no posts.',
+  1, 2, 3 =>  'There are a few posts.',
+  default => 'There are many posts.'
 }
 ?>
-
 <h1><?= $title ?></h1>
 <h2><?= $numberOfPostsText ?></h2>
 <p><?= $message ?></p>
